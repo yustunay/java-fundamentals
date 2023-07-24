aaf12d290f2d9f61cafe3c0b3d68a6bb108f5d07
yahya.ustunay <yahya.ustunay@azoomee.com>
2023-07-24 18:20:13 +0100

    added doubledlinkedlist and delete odd nodes

--- src/main/java/DoubledLinkedListCustom.java
+++ src/main/java/DoubledLinkedListCustom.java
@@ -1,13 +1,15 @@
-public class LinkedListCustom {
+public class DoubledLinkedListCustom {
     Node head;
 
     class Node {
         int data;
         Node next;
+        Node prev;
 
         public Node(int data) {
             this.data = data;
             this.next = null;
+            this.prev = null;
         }
     }
 
@@ -20,7 +22,28 @@ public class LinkedListCustom {
             while (current.next != null) {
                 current = current.next;
             }
+            newNode.prev = current;
             current.next = newNode;
+        }
+    }
+
+    public void delete(int data) {
+        Node current = head;
+        while (current.next != null) {
+
+            if (current.data == data) {
+                Node prevNode = current.prev;
+                Node nextNode = current.next;
+                prevNode.next = nextNode;
+                nextNode.prev = prevNode;
+            }
+
+            current = current.next;
+        }
+
+        if (current.data == data) {
+            Node prevNode = current.prev;
+            prevNode.next = null;
         }
     }
 
@@ -33,11 +56,52 @@ public class LinkedListCustom {
         System.out.println(); // Print a new line after printing all elements
     }
 
+    public void deleteOddNodes() {
+        Node current = head;
+        while (current.next != null) {
+
+            if (current.data % 2 == 1) {
+                Node prevNode = current.prev;
+                Node nextNode = current.next;
+
+                if (prevNode != null)
+                    prevNode.next = nextNode;
+                else
+                    head = nextNode;
+
+                nextNode.prev = prevNode;
+            }
+            current = current.next;
+        }
+
+        if (current.data % 2 == 1) {
+            Node prevNode = current.prev;
+            if (prevNode != null)
+                prevNode.next = null;
+        }
+    }
+
     public static void main(String[] args) {
-        LinkedListCustom list = new LinkedListCustom();
-        list.append(10);
-        list.append(20);
-        list.append(30);
+        DoubledLinkedListCustom list = new DoubledLinkedListCustom();
+        list.append(1);
+        list.append(6);
+        list.append(12);
+        list.append(21);
+        list.append(28);
+        list.append(31);
+        list.append(46);
+        list.append(49);
+        list.append(51);
+        list.append(67);
         list.printList();
+
+        list.deleteOddNodes();
+        list.printList();
+
+//        list.delete(21);
+//        list.printList();
+//
+//        list.delete(31);
+//        list.printList();
     }
 }
